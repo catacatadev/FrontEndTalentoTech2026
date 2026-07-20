d27aa5f0172f9132ad06f3a5766a306320704b3c
Cata <catalinasfilio@hotmail.com>
2026-07-19 22:15:12 -0300

    Agregado renderizado dinámico y correcciones

--- js/index.js
+++ js/index.js
@@ -7,25 +7,29 @@ const renderizarProductos = () => {
     const contenedor = document.getElementById("contenedor-tarjetas"); 
 
     fetch("./data/productos.json") 
-        .then((res) => res.json()) 
+        .then((response) => response.json()) 
         .then((data) => {
-            data.forEach((producto) =>{
+            data.forEach((producto) => {
                 const tarjeta = document.createElement("article"); 
-                tarjeta.classList.add("card", "text-dark"); 
+                tarjeta.classList.add("card", "text-dark", "text-secondary"); 
 
-                const img = createElement("img"); 
+                const img = document.createElement("img"); 
                 img.src = `./${producto.img}`; 
                 img.alt = producto.nombre; 
 
                 const titulo = document.createElement("h3"); 
                 titulo.textContent = producto.nombre; 
 
+                const descripcion = document.createElement("p"); 
+                descripcion.classList.add("card-description"); 
+                descripcion.textContent = producto.descripcion;
+
                 const precio = document.createElement("p"); 
                 precio.textContent = `$${producto.precio}`; 
 
                 const boton = document.createElement("button"); 
                 boton.classList.add("btn", "bg-secondary", "text-dark"); 
-                boton.textContent = "Agregar al carrito";
+                boton.textContent = "Reservar";
 
                 boton.addEventListener("click", () => {
                     agregarAlCarrito(producto);
@@ -34,13 +38,14 @@ const renderizarProductos = () => {
                 //estructura tarjeta 
                 tarjeta.appendChild(img);
                 tarjeta.appendChild(titulo);
+                tarjeta.appendChild(descripcion);
                 tarjeta.appendChild(precio);
                 tarjeta.appendChild(boton);
 
                 //agregar tarjeta al dom 
                 contenedor.appendChild(tarjeta);
 
-            });
+            }); 
               
         })
         .catch((error) => console.log(error)); 
